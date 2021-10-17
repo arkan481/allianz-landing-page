6eeb468ed692ff3d629665f065da2909c1a0865f
arkan481 <arkanharyo@gmail.com>
2021-10-17 21:43:39 +0700

    quiz func for mobile was added

--- js/main.js
+++ js/main.js
@@ -1,49 +1,49 @@
 $(document).ready(function () {
   // setting all other quizzes except the first to be hidden
-  $("#question-box-2").hide();
-  $("#question-box-3").hide();
-  $("#question-box-4").hide();
-  $("#question-box-5").hide();
+  $(".question-box-2").hide();
+  $(".question-box-3").hide();
+  $(".question-box-4").hide();
+  $(".question-box-5").hide();
 
   $(".question-answers__choice--1").click(function () {
     markAnswered(".question-answers__choice--1", this);
-    fadeInElement("#question-box-2");
-    makeOpaque("#question-box-1");
+    fadeInElement(".question-box-2");
+    makeOpaque(".question-box-1");
   });
 
   $(".question-answers__choice--2").click(function () {
     markAnswered(".question-answers__choice--2", this);
-    fadeInElement("#question-box-3");
-    makeOpaque("#question-box-2");
+    fadeInElement(".question-box-3");
+    makeOpaque(".question-box-2");
   });
 
   $(".question-answers__choice--3").click(function () {
     markAnswered(".question-answers__choice--3", this);
-    fadeInElement("#question-box-4");
-    makeOpaque("#question-box-3");
+    fadeInElement(".question-box-4");
+    makeOpaque(".question-box-3");
   });
 
   $(".question-answers__choice--4").click(function () {
     markAnswered(".question-answers__choice--4", this);
-    fadeInElement("#question-box-5");
-    makeOpaque("#question-box-4");
+    fadeInElement(".question-box-5");
+    makeOpaque(".question-box-4");
   });
 
-  $("#question-5-choice-a").click(function () {
+  $(".question-5-choice-a").click(function () {
     markAnswered(".question-answers__choice--5", this);
-    makeOpaque("#question-box-5");
+    makeOpaque(".question-box-5");
     fadeInElement("#dialog-a");
   });
 
-  $("#question-5-choice-b").click(function () {
+  $(".question-5-choice-b").click(function () {
     markAnswered(".question-answers__choice--5", this);
-    makeOpaque("#question-box-5");
+    makeOpaque(".question-box-5");
     fadeInElement("#dialog-b");
   });
 
-  $("#question-5-choice-c").click(function () {
+  $(".question-5-choice-c").click(function () {
     markAnswered(".question-answers__choice--5", this);
-    makeOpaque("#question-box-5");
+    makeOpaque(".question-box-5");
     fadeInElement("#dialog-c");
   });
 
